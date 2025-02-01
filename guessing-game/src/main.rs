a77005a03b1ebefde8eaef5f632917179f7f465a
Haseebullah <haskeebullah265@gmail.com>
2025-02-01 14:48:22 +0500

    Added some comments for context

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,10 +1,18 @@
-use std::io;
+use std::io; //We're calling the input/output library from the std(standard) library. This helps us in getting an input from the user
 
 fn main () {
     
-    println!("Guess the number!");
+    println!("Guess the number!"); // Using println! (macro) to print output on terminal
     println!("Please input your guess:");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); //let is used to create a variable.
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    println!("You guessed: {}", guess);
+
+    
 
 }
